30634d92674172659536986db417faacb20f5a92
Wooseong Lee <leews7752@gmail.com>
2025-01-24 19:14:09 +0900

    more pub

--- README.md
+++ README.md
@@ -2,10 +2,33 @@
 
 ---
 
-### Subject
+## Image Cropping
 
-Apple | Mac Mini.
+### Use Absolute;
 
----
+<Wrapper>
+    Positon: Absolute;
+    W/H: 100%; //wrapping area
 
-### Log
+    <Picture>
+    H: 100%; // Horizontal Croppping
+        <Image>
+        H: 100% // Horizontal Cropping
+        Object-fit: Cover // Crop Func
+
+### Use Absolute/Grid
+
+<Wrapper>
+    Positon: Absolute;
+    Display: Grid;
+    Justify-contents: center;
+    Align-content: center; // Centering <Picture> Item;
+    Alight-items: center; // Centering Pictures <Img> Item;
+    W/H: 100%; //wrapping area
+
+    <Picture>
+    Display: Block;
+    Width: ## px;
+    Height: ## px;
+        <Image>
+        none;
--- scripts/test_script.js
+++ scripts/test_script.js
@@ -1,44 +1,139 @@
 window.addEventListener("DOMContentLoaded", () => {
-	let index = 0;
-	let amount = 0;
-	let isPaused = false;
-	const inc = 1;
-	const delta = 0.1;
-
-	const target = document.querySelector(".dotnav-items");
-	const children = Array.from(target.children);
-	const limit = children.length;
-
-	const loop = requestAnimationFrame(setIndex);
+	// Button
 	const button = document.querySelector(".button");
-
 	button.addEventListener("click", () => {
-		return (isPaused = !isPaused);
+		return (nav.isPaused = !nav.isPaused);
 	});
 
-	function setIndex() {
-		if (index >= limit) return cancelAnimationFrame(loop);
+	const button2 = document.querySelector(".button-re");
+	button2.addEventListener("click", () => {
+		console.log(nav.isReversed);
+		return (nav.isReversed = !nav.isReversed);
+	});
 
-		if (amount >= 100) {
-			index++;
-			amount = 0;
+	const button3 = document.querySelector(".button-to");
+	button3.addEventListener("click", () => {
+		nav.targetIndex = 1;
+		nav.isMoving = true;
+		console.log(nav.isMoving);
+	});
+
+	const loop = requestAnimationFrame(animateNav);
+
+	let nav = {
+		curIndex: 0,
+		targetIndex: 0,
+		amount: 0,
+		spd: 1,
+		isMoving: false,
+		isReversed: false,
+		isPaused: false,
+	};
+
+	const container = document.querySelector(".dotnav-items");
+	const children = Array.from(container.children);
+	const maxAmount = 100;
+	const minAmount = 0;
+
+	// Index Contraint
+	nav.maxIndex = children.length - 1;
+
+	function animateNav() {
+		// 일시정지 해제
+		if (!nav.isPaused) {
+			// 타겟 있음
+			if (nav.isMoving) {
+				nav.spd = 2;
+				checkFlag(nav);
+				updateAmount(nav);
+				updateIndexMoving(nav);
+			}
+			// 타겟 없음
+			else {
+				nav.spd = 0.5;
+				updateAmount(nav);
+				updateIndex(nav);
+			}
+			console.log(nav.curIndex, nav.amount);
+			updateDOM(nav);
+		}
+		// Callback
+		requestAnimationFrame(animateNav);
+	}
+
+	function updateAmount(obj) {
+		// 증감
+		if (obj.amount < maxAmount && !obj.isReversed) obj.amount += obj.spd;
+		if (obj.amount > minAmount && obj.isReversed) obj.amount -= obj.spd;
+
+		// 제한
+		if (obj.amount >= maxAmount) obj.amount = maxAmount;
+		if (obj.amount <= minAmount) obj.amount = minAmount;
+	}
+
+	function updateIndexMoving(obj) {
+		if (obj.amount === maxAmount && !obj.isReversed) {
+			if (obj.curIndex < obj.targetIndex) {
+				obj.curIndex++;
+				obj.amount = minAmount;
+			}
 		}
 
-		if (!isPaused) amount += inc * delta;
-		console.log(amount);
-		const ac = (100 - amount) * -1;
+		if (obj.amount === minAmount && obj.isReversed) {
+			if (obj.curIndex > obj.targetIndex) {
+				obj.curIndex--;
+				obj.isReversed = true;
+				obj.amount = minAmount;
+			}
+		}
 
+		if (obj.curIndex === obj.targetIndex) {
+			obj.isMoving = false;
+			obj.isReversed = false;
+			obj.amount = 0;
+		}
+	}
+
+	function updateIndex(obj) {
+		// 증가
+		if (obj.amount === maxAmount && !obj.isReversed) {
+			// 최대 인덱스에 도달하면 종료
+			if (obj.curIndex === obj.maxIndex) {
+				return;
+			}
+
+			obj.curIndex++;
+			obj.amount = minAmount;
+		}
+
+		// 감소
+		if (obj.amount === minAmount && obj.isReversed) {
+			// 최소 인덱스에 도달하면 종료
+			if (obj.curIndex === 0) {
+				return;
+			}
+
+			obj.curIndex--;
+			obj.amount = maxAmount;
+		}
+	}
+
+	function updateDOM(obj) {
+		const ac = (100 - obj.amount) * -1;
 		children.forEach((item, i) => {
-			if (i == index) {
+			if (i === obj.curIndex) {
 				item.style.setProperty("--dotnav-inc", `${ac.toFixed(2)}%`);
 				item.style.setProperty("--dotnav-width", `56px`);
-				target.style.setProperty("--dontnav-index", index);
 			} else {
 				item.style.setProperty("--dotnav-inc", "-100%");
 				item.style.setProperty("--dotnav-width", "8px");
 			}
 		});
+	}
 
-		requestAnimationFrame(setIndex);
+	function checkFlag(obj) {
+		if (obj.curIndex < obj.targetIndex) obj.isReversed = false;
+		if (obj.curIndex > obj.targetIndex) obj.isReversed = true;
+		else obj.isReversed = false;
 	}
 });
